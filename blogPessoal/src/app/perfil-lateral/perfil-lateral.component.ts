fe4714595b0111a348e62eb503d02efa5517b7e2
unknown <helder825@hotmail.com>
2020-11-04 17:44:26 -0300

    comunicando com o back-end

--- blogPessoal/src/app/perfil-lateral/perfil-lateral.component.ts
+++ blogPessoal/src/app/perfil-lateral/perfil-lateral.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilLateralComponent implements OnInit {
 
+  nome :string = 'Valber Renan'
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    
   }
 
 }
