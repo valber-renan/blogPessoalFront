711aa43103f3add5f8e7ee28ce0c9a7dcc4e067e
unknown <valber_05@hotmail.com>
2020-11-11 23:51:34 -0300

    crud e alert

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule}from '@angular/common/http'
-import { FormsModule}from '@angular/forms'
-import { OrderModule}from 'ngx-order-pipe'
+import { HttpClientModule}from '@angular/common/http';
+import { FormsModule}from '@angular/forms';
+import { OrderModule}from 'ngx-order-pipe';
+import { ModalModule} from 'ngx-bootstrap/modal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +17,11 @@ import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { from } from 'rxjs';
 import { PostTemaComponent } from './post-tema/post-tema.component';
+import { PutPostagemComponent } from './put-postagem/put-postagem.component';
+import { PutTemaComponent } from './put-tema/put-tema.component';
+import { DeletePostagemComponent } from './delete-postagem/delete-postagem.component';
+import { DeleteTemaComponent } from './delete-tema/delete-tema.component';
+import { AlertasComponent } from './alertas/alertas.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +33,12 @@ import { PostTemaComponent } from './post-tema/post-tema.component';
     PerfilLateralComponent,
     LoginComponent,
     CadastroComponent,
-    PostTemaComponent
+    PostTemaComponent,
+    PutPostagemComponent,
+    PutTemaComponent,
+    DeletePostagemComponent,
+    DeleteTemaComponent,
+    AlertasComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +46,8 @@ import { PostTemaComponent } from './post-tema/post-tema.component';
     FontAwesomeModule,
     HttpClientModule,
     FormsModule,
-    OrderModule
+    OrderModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
